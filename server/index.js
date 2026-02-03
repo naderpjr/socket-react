51e5636472bb96083240f492fae1c82c9680905e
Nader PJR <pjrnader@gmail.com>
2026-02-03 23:36:42 +0430

    server config created

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+server.listen(3001, () => {
+    console.log("Server is running");
+})
